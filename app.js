1adde69a92e53cf085536db56ff63a2976e90b82
jamesjo <jamescakr@gmail.com>
2024-10-25 15:05:14 -0600

    update for assignment and remove .env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index")
 const app = express();
 
 require("dotenv").config();
@@ -9,13 +10,15 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use("/api", indexRouter)
+
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(() => console.log("mongoose connected"))
   .catch((err) => console.log("DB connection failed", err));
 
-  app.listen(process.env.PORT || 5000, ()=> {
+  app.listen(process.env.PORT || 4000, ()=> {
     console.log("server on");
     
   })
