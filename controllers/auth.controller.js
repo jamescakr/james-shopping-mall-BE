fbe27d36cbf2a7f13156dcd645831dcc20d5ace3
jamesjo <jamescakr@gmail.com>
2024-10-26 17:06:47 -0600

    assignment 7-8

---
+++ controllers/auth.controller.js
@@ -0,0 +1,21 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs")
+
+const authController = {};
+
+authController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        // token
+        const token = await user.generateToken();
+        return res.status(200).json({status: "success", user, token})
+      }
+    }
+  } catch (error) {
+    res.status(400).json({status: "failed", error:error.message})
+  }
+};
--- models/User.js
+++ models/User.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const userSchema = Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
     level: { type: String, default: "customer" } 
-}, { timestamps: true });
+}, { timestamps: true } );
 
 userSchema.methods.toJSON = function () {
     const obj = this._doc;
@@ -16,6 +18,13 @@ userSchema.methods.toJSON = function () {
     delete obj.createAt;
     return obj
 };
+
+userSchema.methods.generateToken = async function () {
+    const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return token;
+  };
 
 const User = mongoose.model("User", userSchema)
 module.exports = User;
